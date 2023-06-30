d9e7587863f61eba3f6acf09d33411c23fe46a8e
Stephan Meijer <stephan.meijer@gmail.com>
2023-06-30 15:00:46 +0200

    magicbell-cli 1.2.0

--- Formula/magicbell-cli.rb
+++ Formula/magicbell-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MagicbellCli < Formula
   desc "Work with MagicBell from the command-line"
   homepage "https://magicbell.com"
-  url "https://registry.npmjs.org/@magicbell/cli/-/cli-1.1.0.tgz"
-  sha256 "6531122609494b340bd6e0a1a7a4b1c4bd9428624a639f2a82a7b180ea45bc7e"
+  url "https://registry.npmjs.org/@magicbell/cli/-/cli-1.2.0.tgz"
+  sha256 "8b216eb1a6cc9cbe5c2c59664937b6714601bbbeb8980c9933f9b9de81b07ada"
   license :cannot_represent # SEE LICENSE IN LICENSE
 
   livecheck do
