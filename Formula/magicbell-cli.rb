60a00754cca5a3da78dc2689396ce54dbee01f48
Stephan Meijer <stephan.meijer@gmail.com>
2023-07-13 10:34:10 +0200

    magicbell-cli 2.0.3

--- Formula/magicbell-cli.rb
+++ Formula/magicbell-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MagicbellCli < Formula
   desc "Work with MagicBell from the command-line"
   homepage "https://magicbell.com"
-  url "https://registry.npmjs.org/@magicbell/cli/-/cli-1.3.0.tgz"
-  sha256 "ee8cf6104850c036966560e532e449d589ad42f7dab0f30f2a87776f510d4bc0"
+  url "https://registry.npmjs.org/@magicbell/cli/-/cli-2.0.3.tgz"
+  sha256 "9a2af9094382e26b8d3f7e5accb512ae05a6e3f91c1f94de21f9f7e7b917c9f1"
   license :cannot_represent # SEE LICENSE IN LICENSE
 
   livecheck do
@@ -16,11 +16,11 @@ class MagicbellCli < Formula
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir[libexec/"bin/*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
     assert_match(version.to_s, shell_output("#{bin}/magicbell --version"))
-    assert_match(/you are not logged in/i, shell_output("#{bin}/magicbell logout"))
+    assert_match(/you are not logged in./i, shell_output("#{bin}/magicbell logout"))
   end
 end
