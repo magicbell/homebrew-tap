4316a58bafab7a842404571933e5e2e43abff583
MagicBella <137158696+MagicBella@users.noreply.github.com>
2024-09-16 16:43:36 +0200

    update Formula/magicbell-cli.rb

--- Formula/magicbell-cli.rb
+++ Formula/magicbell-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MagicbellCli < Formula
   desc "Work with MagicBell from the command-line"
   homepage "https://magicbell.com"
-  url "https://registry.npmjs.org/@magicbell/cli/-/cli-3.5.0.tgz"
-  sha256 "afc358c4b4b375f86766ca2ac497321ee9d4485094c72e2f88f1bbfdcd9e1e66"
+  url "https://registry.npmjs.org/@magicbell/cli/-/cli-4.0.1.tgz"
+  sha256 "f2eda7250ef465136065df7d3593577c91ca670eca089bee362ec332bfc9048e"
   license :cannot_represent # SEE LICENSE IN LICENSE
 
   livecheck do
@@ -15,7 +15,7 @@ class MagicbellCli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *Language::Node.std_npm_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
