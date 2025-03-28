ca73f1247afcbaa844156ea817fe1bdc9cf2cfb5
MagicBella <137158696+MagicBella@users.noreply.github.com>
2025-03-28 15:54:38 +0100

    update Formula/magicbell-cli.rb

--- Formula/magicbell-cli.rb
+++ Formula/magicbell-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class MagicbellCli < Formula
   desc "Work with MagicBell from the command-line"
   homepage "https://magicbell.com"
-  url "https://registry.npmjs.org/@magicbell/cli/-/cli-4.2.3.tgz"
-  sha256 "9a583777e90b1e9aea7937f346db6d8b50b53100ed547f1e191deedee4c796f9"
+  url "https://registry.npmjs.org/@magicbell/cli/-/cli-4.2.4.tgz"
+  sha256 "6a1522044887d4cb90cf34dfc130cc2d618f02b9a3704349c2c0641ab6f06a23"
   license :cannot_represent # SEE LICENSE IN LICENSE
 
   livecheck do
